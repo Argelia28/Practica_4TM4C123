57fe623a51334e9dfa3663f1fe2472f18a0b73e9
Argelia28 <112964372+Argelia28@users.noreply.github.com>
2023-01-26 12:26:08 -0600

    Update

--- GPIO.c
+++ GPIO.c
@@ -1,23 +1,43 @@
 #include "lib/include.h"
 extern void Configurar_GPIO(void)
 {
-    SYSCTL->RCGCGPIO |=(1<<5); 
-    GPIOF->LOCK= 0x4C4F434B;   // 2) unlock GPIO Port F
-    GPIOF->CR = 0x1F;           // allow changes to PF4-0
-    GPIOF->AMSEL = 0x00;        // 3) disable analog on PF
-    GPIOF->PCTL = 0x00000000;   // 4) PCTL GPIO on PF4-0
-    GPIOF->DIR = 0x0E;          // 5) PF4,PF0 in, PF3-1 out
-    GPIOF->AFSEL = 0x00;        // 6) disable alt funct on PF7-0
-    GPIOF->PUR = 0x11;          // enable pull-up on PF0 and PF4
-    GPIOF->DEN = 0x1F;          // 7) enable digital I/O on PF4-0
+    SYSCTL->RCGCGPIO |=(1<<3); 
+    GPIOD->LOCK= 0x4C4F434B;   // 2) unlock GPIO Port D
+    GPIOD->CR = 0xF3;           // allow changes to PD0-1 4-7
+    GPIOD->AMSEL = ~0xF3;        // 3) disable analog
+    GPIOD->PCTL = ~0xF3;          // 4) PCTL GPIO on PD
+    GPIOD->DIR = ~0xF3;          // 5) PD0, PD1, PD4, PD5, PD6, PD7 input=0
+    GPIOD->AFSEL = ~0xF3;        // 6) disable alt funct on PD
+    GPIOD->PUR = 0xF3;          // enable pull-up
+    GPIOD->DEN = 0xF3;          // 7) enable digital I/O on PD
+
+    GPIOD->IM = ~0xF3;         //pin d0-d1 y d4-d7 MASK
+    GPIOD->IS = ~0xF3;          //POR FLANCO
+    GPIOD->IBE = ~0xF3;          //configurado por evento, un flanco
+    GPIOD->IEV = ~0xF3;         //por flanco si fuera 1 es por nivel
+    GPIOD->IM = 0xF3;           //UNMASK
+
+    /*GPIOD es la interrupcion 3
+    n=integer(interrupt number/4)
+      =integer(3/4)=0.75=0
+    p=(interrupt number % 4)
+      =(3%4)=3 -> pin 31-29
+      4n+3 *duda*
+      p=# interrupcion
+    */
+    NVIC->IP[0] = (NVIC->IP[0]&0x0FFFFFFF) | 0x30000000; //pin 29-31 y prioridad
+    //NVIC->IP[0] = (NVIC->IP[0]&0xFFFFFFF0) | 0x00000003; //pin 3 y prioridad *duda*
+    /*
+    n=interrupt number/32
+      =3/32=0
+    b=interrupt number%32
+      =3
+    */
+    NVIC->ISER[0]=(0UL<<3); //EN0 register and b is the pin
+    
 }
 
 extern void Delay(void)
 {
-  unsigned long volatile time;
-  time = 1600000;
-  while(time)
-  {
-		time--;
-  }
+  GPIOD->ICR=0xF3; //Limpiar la interrupcion
 }
--- startup_gcc.c
+++ startup_gcc.c
@@ -36,6 +36,7 @@ static void NmiSR(void);
 static void FaultISR(void);
 static void IntDefaultHandler(void);
 
+extern void Delay(void);
 //*****************************************************************************
 //
 // The entry point for the application.
@@ -79,7 +80,7 @@ void (* const g_pfnVectors[])(void) =
     IntDefaultHandler,                      // GPIO Port A
     IntDefaultHandler,                      // GPIO Port B
     IntDefaultHandler,                      // GPIO Port C
-    IntDefaultHandler,                      // GPIO Port D
+    Delay,                      // GPIO Port D
     IntDefaultHandler,                      // GPIO Port E
     IntDefaultHandler,                      // UART0 Rx and Tx
     IntDefaultHandler,                      // UART1 Rx and Tx
